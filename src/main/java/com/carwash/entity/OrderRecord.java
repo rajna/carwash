326530eee86a245162ead48f929ee9b6b356b82d
ilvelh <ilvelh@icloud.com>
2014-11-11 17:27:48 +0800

    订单修改数据模型

---
+++ src/main/java/com/carwash/entity/OrderRecord.java
@@ -0,0 +1,45 @@
+/*
+ *	               _ooOoo_ 
+ *	              o8888888o 
+ *	              88" . "88 
+ *	              (| -_- |) 
+ *	              O\  =  /O 
+ *	           ____/`---'\____ 
+ *	         .'  \\|     |//  `. 
+ *	        /  \\|||  :  |||//  \ 
+ *	       /  _||||| -:- |||||-  \ 
+ *	       |   | \\\  -  /// |   | 
+ * 	       | \_|  ''\---/''  |   | 
+ * 	       \  .-\__  `-`  ___/-. / 
+ * 	    ___ `. .'  /--.--\  `. . __ 
+ *	  ."" '<  `.___\_<|>_/___.'  >'"". 
+ *	 | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+ *	 \  \ `-.   \_ __\ /__ _/   .-` /  / 
+ *======`-.____`-.___\_____/___.-`____.-'====== 
+ *                   `=---=' 
+ *^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+ *佛祖保佑       永无BUG 
+ * File name:          OrderRecord.java
+ * Copyright@blog.ilvelh.com(China)
+ * Editor:           JDK1.7_40
+ */
+package com.carwash.entity;
+
+import java.util.Date;
+
+/**
+ * 订单修改记录
+ * <p>
+ * Author: ilvelh
+ * <p>
+ * Date:2014-11-11 Time:下午5:22:42
+ * <p>
+ */
+public class OrderRecord {
+	private int id;
+	private int orderId;
+	private int userId;
+	private String userName;
+	private Date create_date = new Date();
+	private String description;
+}
