2b5375b6f9acd55d3c9c0500a24f2d00475cbde6
ilvelh <ilvelh@icloud.com>
2014-11-11 17:56:50 +0800

    修正实体类注解

--- src/main/java/com/carwash/entity/Order.java
+++ src/main/java/com/carwash/entity/Order.java
@@ -30,10 +30,11 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Length;
@@ -110,8 +111,8 @@ public class Order {
 		this.address = address;
 	}
 
-	@ManyToOne
-	@JoinColumn(name = "orderId")
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "oid")
 	public Set<OrderItem> getOrderItems() {
 		return orderItems;
 	}
