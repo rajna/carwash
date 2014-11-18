f75b27403a6703d28b12466b4423cfd56f9bdbe6
ilvelh <ilvelh@icloud.com>
2014-11-18 16:58:42 +0800

    ctrl+shift+o

--- src/main/java/com/carwash/ctrl/Api.java
+++ src/main/java/com/carwash/ctrl/Api.java
@@ -29,7 +29,6 @@ import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.springframework.asm.commons.Method;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
